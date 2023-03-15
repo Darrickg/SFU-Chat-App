57d6ea21f6a233457f1de8599162a5c9acafb9c2
aiyab <aiya_bowman@sfu.ca>
2023-03-15 05:14:21 +0000

    Upload New File

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="SFU logo" src="./assets/SFU_block.png">
+  <AppNav>
+  </AppNav>
+</template>
+
+<script>
+import AppNav from './components/AppNav.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppNav
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
