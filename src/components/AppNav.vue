36767819b99ac73f3f3fab688f8a27ed1fb3c3a4
aiyab <aiya_bowman@sfu.ca>
2023-03-15 04:50:36 +0000

    Upload New File

---
+++ src/components/AppNav.vue
@@ -0,0 +1,19 @@
+<!--
+    On all pages (at the top?)
+    List all page sections:
+        Home
+        Settings
+        Log Out
+        ...whatever else we come up with
+-->
+<template>
+    
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
