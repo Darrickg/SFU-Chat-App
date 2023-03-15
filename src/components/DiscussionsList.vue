e64560818e2642b2b941d7b67e4b9e594cc6f8e2
aiyab <aiya_bowman@sfu.ca>
2023-03-15 04:51:41 +0000

    Upload New File

---
+++ src/components/DiscussionsList.vue
@@ -0,0 +1,19 @@
+<!--
+    On course's main page
+    List of all discussions, main feed
+    Can list by course name, recent activity
+    Shows 3 most recent replies (or snippet of them)
+    Can pin courses
+    Next to each course is direct link to the 3 most recent discussions? Would be convenient
+-->
+<template>
+    
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
