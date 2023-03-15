22ef81956bd695bf1a1b9b0885e5f0b095cb43ec
aiyab <aiya_bowman@sfu.ca>
2023-03-15 04:50:47 +0000

    Upload New File

---
+++ src/components/ChatsList.vue
@@ -0,0 +1,19 @@
+<!--
+    On homepage, main feed of messages
+    Not servers section of Discord, more main page of reddit/fb
+    List of chats user is in
+    Can list by course name, recent activity
+    Can pin courses
+    Next to each course is direct link to the 3 most recent discussions? Would be convenient
+-->
+<template>
+    
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
