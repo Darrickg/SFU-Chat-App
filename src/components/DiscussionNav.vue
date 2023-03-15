7a264189a3e8a2c0d51f20831447d4c715e3e920
aiyab <aiya_bowman@sfu.ca>
2023-03-15 04:51:05 +0000

    Upload New File

---
+++ src/components/DiscussionNav.vue
@@ -0,0 +1,22 @@
+<!--
+    On all course pages
+    List all course discussions, ie:
+        Assignment 1
+        Assignment 2
+        Midterm
+    Channels section of Discord
+    Profs/TAs can add new discussions
+    Profs/TAs have course subnav for all sections
+    Can pin and mute discussions from nav
+-->
+<template>
+    
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
