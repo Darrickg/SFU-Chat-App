c48be939b3dd0e34abffa065c286e19a6bde5d1f
aiyab <aiya_bowman@sfu.ca>
2023-03-15 04:52:23 +0000

    Upload New File

---
+++ src/components/MessagesList.vue
@@ -0,0 +1,17 @@
+<!--
+    Shows all sent messages
+    Username, date, message
+    Edit/delete buttons for user's own messages
+    Profs/TAs can delete any messages
+-->
+<template>
+    
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
