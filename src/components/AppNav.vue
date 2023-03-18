f169f182dab6cef2a5db92d28fb16921cdf07ebb
aiyab <aiya_bowman@sfu.ca>
2023-03-17 22:01:03 -0700

    Primitive list of discussions added

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -7,9 +7,9 @@
         ...whatever else we come up with
 -->
 <template>
-  <section class="container flex flex-wrap bg-sfu-dark-red">
+  <section class="container flex flex-wrap bg-sfu-dark-red" w-full>
     <img alt="SFU logo" src="@/assets/SFU_block.png" class="h-14 w-auto">
-    <button @click="isHidden=!isHidden" class="text-white hover:text-sfu-dark-red hover:bg-white">Account</button>
+    <button @click="isHidden=!isHidden" class="absolute right-0 text-white hover:text-sfu-dark-red hover:bg-white">Account</button>
     <nav id="accountNav" v-if="!isHidden" class="container flex flex-wrap bg-sfu-dark-red">
       <router-link to="/" class="text-white hover:text-sfu-dark-red hover:bg-white">Home</router-link>
       <router-link to="/settings" class="text-white hover:text-sfu-dark-red hover:bg-white">Settings</router-link>
