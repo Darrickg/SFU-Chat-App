c8c6d4b704db74f4a4e526b77a2c1723a88a74ae
aiyab <aiya_bowman@sfu.ca>
2023-04-01 21:23:20 -0700

    edit button fleshed out, more message info added

--- src/components/Messages/MessagesList.vue
+++ src/components/Messages/MessagesList.vue
@@ -6,11 +6,13 @@
 -->
 <template>
   <ul v-for="message in messages" :key="message.id" class="container flex flex-wrap bg-gray-100">
-      <li class="text-black">
-        <p>{{message.content}}</p>
-        <button>Reply</button>
-        <button v-if="ownMessage">✎</button>
-        <button v-if="ownMessage || isInstructor">X</button>
+      <li :id="'mes'+message.id" class="text-black p-2">
+        <p v-if="message.reply" v-text="isReply(message.reply)" class="bg-gray-400"></p>
+        <p :id="'text'+message.id">{{message.text}}</p>
+        <p>{{message.email}}</p>
+        <button :id="'reply'+message.id">Reply</button>
+        <button v-on:click="edit(message.id)" :id="'edit'+message.id" v-if="ownMessage">✎</button>
+        <button :id="'del'+message.id" v-if="ownMessage || isInstructor">X</button>
       </li>
   </ul>
 </template>
@@ -21,21 +23,62 @@ export default {
   name: 'MessagesList',
   setup(){
     const messages=ref([//get messages from api
-      {content:"Can I get an extension? Pls?"},
-      {content:"How do you center a div?"},
-      {content:"Angular sucks."},
+      {text:"Can I get an extension? Pls?", id:3, email:"aiya@sfu.ca"},
+      {text:"How do you center a div?", id:4, email:"sarah@sfu.ca"},
+      {text:"Angular sucks.", id:5, email:"darrick@sfu.ca"},
+      {text:"Lol no.", id:6, reply:3, email:"bobby@sfu.ca"}
     ]);
     let ownMessage=new Boolean(true);
     let isInstructor=new Boolean(true);
     return{messages, ownMessage, isInstructor};
-
   },
-  props: {
+  methods:{
+    reply(id){
 
+
+    },
+
+    isReply(replyId){
+      let reply="REPLY ID NOT FOUND. 'TIS BAD.";
+      for(let i=0;i<this.messages.length;i++)
+      {
+        if(this.messages[i].id==replyId)
+        {
+          reply=this.messages[i].text;
+          console.log("fuck."+reply);
+        }
+      }
+      console.log(this.messages[1].id);
+      //shorten to first 20 characters
+      console.log("thingy: "+"text"+replyId.toString());
+      if(reply!=null){
+        return(reply);
+      }
+      else{
+        return("REPLY ID NOT FOUND. 'TIS BAD.");
+        //return(reply.textContent);
+      }
+    },
+
+    edit(id){
+      if(document.getElementById("newTextarea")){
+        document.getElementById("newTextarea").remove();//remove other reply/edit boxes
+      };
+      const edit=document.createElement("textarea");
+      const message=document.getElementById("mes"+id);
+      const oldText=document.getElementById("text"+id);
+      edit.id="newTextarea";
+      edit.textContent=oldText.textContent;
+      edit.classList.add("bg-white");
+      message.appendChild(edit);
+    }
   }
 };
 </script>
 
 <style scoped>
+  .textarea{
+    background-color:white;
+  }
 
 </style>
