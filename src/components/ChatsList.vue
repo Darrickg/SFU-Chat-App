97d03574effffabbbbf9e5fa3673bfbb561a8c8f
dga48 <darrickgunawan@gmail.com>
2023-04-05 23:36:14 -0700

    changed routing mistake

--- src/components/ChatsList.vue
+++ src/components/ChatsList.vue
@@ -49,7 +49,7 @@ export default {
   },
   methods: {
         goToCourse() {
-            this.$router.push({ name: 'course' })
+            this.$router.push({ name: 'discussion' })
         },
     }
 }
